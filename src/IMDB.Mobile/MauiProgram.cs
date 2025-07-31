cfd42d3ce17829059a6eebab8a9703be771f7c45
usuario-27554 <will.dutra26@gmail.com>
2025-07-31 11:18:14 -0300

    feat: inclusão da estilização da tab bar

--- src/IMDB.Mobile/MauiProgram.cs
+++ src/IMDB.Mobile/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace IMDB.Mobile
 {
@@ -9,6 +10,7 @@ namespace IMDB.Mobile
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
