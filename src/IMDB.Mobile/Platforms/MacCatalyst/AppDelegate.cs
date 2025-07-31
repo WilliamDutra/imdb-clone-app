22ea38b9d517cdce1580544e69d8cf9b37e972e3
usuario-27554 <will.dutra26@gmail.com>
2025-07-31 10:35:34 -0300

    criação do projeto

---
+++ src/IMDB.Mobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace IMDB.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
