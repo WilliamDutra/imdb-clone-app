dea5a558bdb5ab8705518d24bef7b8c661cb8432
usuario-27554 <will.dutra26@gmail.com>
2025-07-31 16:11:52 -0300

    feat: inclusão da avaliação do componente card

--- src/IMDB.Mobile/Components/Cards/CardComponent.xaml.cs
+++ src/IMDB.Mobile/Components/Cards/CardComponent.xaml.cs
@@ -4,21 +4,29 @@ public partial class CardComponent : ContentView
 {
 	public static readonly BindableProperty TitleProperty = BindableProperty.Create("Title", typeof(string), typeof(CardComponent), string.Empty);
 
-	public static readonly BindableProperty ImageProperty = BindableProperty.Create("Image", typeof(string), typeof (CardComponent), string.Empty);
+	public static readonly BindableProperty ImageProperty = BindableProperty.Create("Image", typeof(ImageSource), typeof (CardComponent), null);
 
-	public string Title
+    public static readonly BindableProperty RatingProperty = BindableProperty.Create("Rating", typeof(string), typeof(CardComponent), string.Empty);
+
+    public string Title
 	{
 		get => (string)GetValue(TitleProperty);
 		set => SetValue(TitleProperty, value);
 	}
 
-	public string Image
+	public ImageSource Image
 	{
-		get => (string)GetValue(ImageProperty);
+		get => (ImageSource)GetValue(ImageProperty);
 		set => SetValue(ImageProperty, value);
 	}
 
-	public CardComponent()
+    public string Rating
+    {
+        get => (string)GetValue(RatingProperty);
+        set => SetValue(RatingProperty, value);
+    }
+
+    public CardComponent()
 	{
 		InitializeComponent();
 	}
---
+++ src/IMDB.Mobile/Resources/Styles/Handlers/TabBarCustomHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDB.Mobile.Resources.Styles.Handlers
+{
+    public class TabBarCustomHandler
+    {
+        public static void Customize()
+        {
+            Microsoft.Maui.Handlers.TabbedViewHandler.Mapper.Add("TabBarCustomHandler", (handler, tab) =>
+            {
+
+            });
+        }
+    }
+}
