f269cac7cb5fd6ff10a57633117e9f11399d097f
usuario-27554 <will.dutra26@gmail.com>
2025-08-01 09:08:05 -0300

    feat: inclusão do componente card com descrição

---
+++ src/IMDB.Mobile/Components/Cards/CardWithDescriptionComponent.xaml.cs
@@ -0,0 +1,44 @@
+using System.Collections.ObjectModel;
+
+namespace IMDB.Mobile.Components.Cards;
+
+public partial class CardWithDescriptionComponent : ContentView
+{
+    public static readonly BindableProperty TitleProperty = BindableProperty.Create("Title", typeof(string), typeof(CardComponent), string.Empty);
+
+    public static readonly BindableProperty ImageProperty = BindableProperty.Create("Image", typeof(ImageSource), typeof(CardComponent), null);
+
+    public static readonly BindableProperty RatingProperty = BindableProperty.Create("Rating", typeof(string), typeof(CardComponent), string.Empty);
+
+    public static readonly BindableProperty DescriptionProperty = BindableProperty.Create("Description", typeof(string), typeof(CardComponent), string.Empty);
+
+    public string Title
+    {
+        get => (string)GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+
+    public ImageSource Image
+    {
+        get => (ImageSource)GetValue(ImageProperty);
+        set => SetValue(ImageProperty, value);
+    }
+
+    public string Rating
+    {
+        get => (string)GetValue(RatingProperty);
+        set => SetValue(RatingProperty, value);
+    }
+
+    public string Description
+    {
+        get => (string)GetValue(DescriptionProperty);
+        set => SetValue(DescriptionProperty, value);
+    }
+
+    public CardWithDescriptionComponent()
+	{
+		InitializeComponent();
+	}
+
+}
