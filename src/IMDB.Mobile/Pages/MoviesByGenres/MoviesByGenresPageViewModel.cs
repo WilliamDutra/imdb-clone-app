1bb1d90069319c6aa90e95b95cfd8b44c86c887c
usuario-27554 <will.dutra26@gmail.com>
2025-08-08 09:45:36 -0300

    fix: ajuste na páginação

--- src/IMDB.Mobile/Pages/MoviesByGenres/MoviesByGenresPageViewModel.cs
+++ src/IMDB.Mobile/Pages/MoviesByGenres/MoviesByGenresPageViewModel.cs
@@ -21,7 +21,11 @@ namespace IMDB.Mobile.Pages.MoviesByGenres
         [ObservableProperty]
         private ObservableCollection<Movie> movies;
 
-        private int TotalPages  = 0;
+        private int _TotalPages  = 0;
+
+        private int _CurrentPage = 1;
+
+        private int _GenreId = 0;
 
         public MoviesByGenresPageViewModel(IGetMoviesByGenres getMoviesByGenres)
         {
@@ -31,20 +35,43 @@ namespace IMDB.Mobile.Pages.MoviesByGenres
         public void ApplyQueryAttributes(IDictionary<string, object> query)
         {
             int genreId = (int)query["genreId"];
+            _GenreId = genreId;
             EachMoviesByGenres(genreId);
         }
 
         [RelayCommand]
         public async void RemainingItems()
         {
-            await Toast.Make($"página {TotalPages = TotalPages - 1}", CommunityToolkit.Maui.Core.ToastDuration.Short).Show();
+            await Toast.Make($"carregando novos filmes", CommunityToolkit.Maui.Core.ToastDuration.Long).Show();
+
+            if (_CurrentPage > _TotalPages)
+                return;
+
+            await Task.Delay(3000);
+            
+            var results = await _getMoviesByGenres.Execute(_GenreId, _CurrentPage);
+            Movies.AddRange(MovieMapper.ToMap(results.Data));
+            await Toast.Make($"página {_CurrentPage}", CommunityToolkit.Maui.Core.ToastDuration.Long).Show();
+            _CurrentPage++;
         }
 
         private async void EachMoviesByGenres(int genreId)
         {
             var results = await _getMoviesByGenres.Execute(genreId);
-            TotalPages = results.TotalPages;
+            _TotalPages = results.TotalPages;
             Movies = MovieMapper.ToMap(results.Data);
         }
     }
+
+    public static class Extensions
+    {
+        public static void AddRange<T>(this ObservableCollection<T> lista, IEnumerable<T> items)
+        {
+            foreach (var item in items)
+            {
+                lista.Add(item);
+            }
+        }
+    }
+
 }
