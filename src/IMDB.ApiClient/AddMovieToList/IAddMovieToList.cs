1eaf6e98f19c0543e9c9f35015001f8a13636069
usuario-27554 <will.dutra26@gmail.com>
2025-08-15 12:23:56 -0300

    feat: inclusão da adição de filmes em listas

---
+++ src/IMDB.ApiClient/AddMovieToList/IAddMovieToList.cs
@@ -0,0 +1,15 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDB.ApiClient.AddMovieToList
+{
+    public interface IAddMovieToList
+    {
+        [Post("/list/{listId}/add_item?session_id={sessionId}")]
+        Task Execute(string sessionId, string listId, [Body] AddMovie request);
+    }
+}
