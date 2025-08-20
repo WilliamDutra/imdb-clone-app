1f34be9fb52daea3198704916516b179450ac17d
usuario-27554 <will.dutra26@gmail.com>
2025-08-20 12:29:53 -0300

    fix: inclusão de 'rating' da api

--- src/IMDB.ApiClient/Mappings/MovieMapper.cs
+++ src/IMDB.ApiClient/Mappings/MovieMapper.cs
@@ -13,7 +13,7 @@ namespace IMDB.ApiClient.Mappings
             foreach (var item in response)
             {
 
-                movies.Add(Movie.Restore(item.Id, item.Title, item.Overview, $"https://image.tmdb.org/t/p/original{item.PosterPath}"));
+                movies.Add(Movie.Restore(item.Id, item.Title, item.Overview, $"https://image.tmdb.org/t/p/original{item.PosterPath}", item.VoteCount));
             }
 
             return movies;
@@ -21,7 +21,7 @@ namespace IMDB.ApiClient.Mappings
 
         public static Movie ToMap(MovieByIdResponse response)
         {
-            var movie = Movie.Restore(response.Id, response.Title, response.Overview, $"https://image.tmdb.org/t/p/original{response.Poster}");
+            var movie = Movie.Restore(response.Id, response.Title, response.Overview, $"https://image.tmdb.org/t/p/original{response.Poster}", 0);
             return movie;
         }
 
