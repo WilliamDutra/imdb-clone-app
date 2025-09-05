aa075c59642b8b544d7db90824eab1cd360a489a
usuario-27554 <will.dutra26@gmail.com>
2025-09-05 16:03:43 -0300

    fix: inclusão do IsLoading na componentização dos titulos da home page

--- src/IMDB.Mobile/Components/Labels/TitleComponent.xaml.cs
+++ src/IMDB.Mobile/Components/Labels/TitleComponent.xaml.cs
@@ -5,10 +5,18 @@ public partial class TitleComponent : ContentView
 
 	public static readonly BindableProperty TextValueProperty = BindableProperty.Create(nameof(TextValue), typeof(string), typeof(TitleComponent), string.Empty);
 
+	public static readonly BindableProperty IsLoadingProperty = BindableProperty.Create(nameof(IsLoading), typeof(bool), typeof(TitleComponent), default(bool));
+
 	public string TextValue
 	{
 		get => (string)GetValue(TextValueProperty);
 		set => SetValue(TextValueProperty, value);
+	}
+
+	public bool IsLoading
+	{
+		get => (bool)GetValue(IsLoadingProperty);
+		set => SetValue(IsLoadingProperty, value);
 	}
 
 	public TitleComponent()
---
+++ src/IMDB.Mobile/Components/Skeletons/SkeletonView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDB.Mobile.Components.Skeletons
+{
+    public class SkeletonView : BoxView
+    {
+        public SkeletonView()
+        {
+            Color = Color.FromHex("#c0c0c0");
+            var smoothAnimation = new Animation();
+
+            smoothAnimation.WithConcurrent(f => Opacity = f, start: 0.3, end: 1, Easing.SinIn);
+            smoothAnimation.WithConcurrent(f => Opacity = f, start: 1, end: 0.3, Easing.SinInOut);
+
+            this.Animate(name: "FadeInOut",
+                animation: smoothAnimation,
+                rate: 20,
+                length: 1500,
+                easing: Easing.SinIn,
+                finished: null,
+                repeat: () => true);
+        }
+    }
+}
