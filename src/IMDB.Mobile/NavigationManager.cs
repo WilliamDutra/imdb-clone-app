381ff673ea59cb34e2b2630560e0129e6090ee2a
usuario-27554 <will.dutra26@gmail.com>
2025-09-18 14:50:12 -0300

    fix: ajuste nos estilos dos componentes

--- src/IMDB.Mobile/NavigationManager.cs
+++ src/IMDB.Mobile/NavigationManager.cs
@@ -8,10 +8,10 @@ namespace IMDB.Mobile
 {
     public class NavigationManager : INavigationManager
     {
-        public Task GoToPage(string pageName) => Shell.Current.GoToAsync(pageName);
+        public Task GoToPage(string pageName) => Shell.Current.GoToAsync(pageName, true);
         
 
-        public Task GoToPage(string pageName, Dictionary<string, object> @params) => Shell.Current.GoToAsync(pageName, @params);
+        public Task GoToPage(string pageName, Dictionary<string, object> @params) => Shell.Current.GoToAsync(pageName, animate: true, parameters: @params);
 
     }
 }
